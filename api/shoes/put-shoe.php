49baab0c841169d1db7fd813d3b8ceb6c6fbbe94
Adam Hegedus <adam.hegedus@yandex.ru>
2018-12-08 20:50:32 +0100

    Add endpoint for shoe edit API

---
+++ api/shoes/put-shoe.php
@@ -0,0 +1,31 @@
+<?php 
+    include_once("../../connection/dbc.php");
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        $id = $data['id'];
+        $brandId = $data['brandId'];
+        $typeId = $data['typeId'];
+        $product = $data['product'];
+
+        $query = "UPDATE shoes SET `typeId`=?, `brandId`=?, `name`=?) WHERE id=?";
+        
+        $stmt = $mysqli -> prepare($query);
+
+        $stmt -> bind_param("iisi", $typeId, $brandId, $product, $id);
+        
+        if ($stmt -> execute()) { 
+            $result = (object) [
+                'data' => $data
+            ];
+        } else {
+            $result = (object) [
+                'data' => null,
+                'meta' => $stmt -> error
+            ];
+        }
+
+        $stmt -> close();
+
+        header('Content-Type: application/json');
+    	echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+?>
